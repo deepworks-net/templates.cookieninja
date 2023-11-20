0d84ceec21badb4d71140d5ebf42b2dfc3a99ea8
Matthew Buske <matt@deepworks.net>
2023-11-19 21:26:58 -0500

    Updates
    Structural updates that fixed some structural issues

--- template/hooks/pre_gen_project.py
+++ template/hooks/pre_gen_project.py
@@ -1,7 +1,9 @@
 import os
 import shutil
 
-def merge_directories(source_dir, target_dir):
+target_directory = '../{{ cookiecutter._template }}{% raw %}{{cookiecutter.project_slug}}/{% endraw %}'
+
+def merge_directories(source_dir, target_dir = target_directory):
     # Create target directory if it does not exist
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
@@ -9,21 +11,25 @@ def merge_directories(source_dir, target_dir):
     # Iterate over all the files in the source directory
     for filename in os.listdir(source_dir):
         source_file = os.path.join(source_dir, filename)
-
+        target_file = os.path.join(target_dir, filename)
         # Check if it's a file and not a directory
         if os.path.isfile(source_file):
-            target_file = os.path.join(target_dir, filename)
-
             # Copy the file to the target directory
             shutil.copy2(source_file, target_file)
             print(f"Copied {source_file} to {target_file}")
+        else:
+            merge_directories(source_file, target_file)
+            print(f"Copied Directory {source_file} to {target_file}")
 
+print(f"Starting pre-generation script")
 
-print(f"Starting pre-generation-script")
-
-target_directory = '.'
 skel_directory = '../{{ cookiecutter._template }}skel/'
-docker_directory = skel_directory+'docker/'
+core_directory = '{skel_directory}core_files/'
+docker_directory = '{skel_directory}docker/'
+
+shutil.rmtree(target_directory, ignore_errors=True)
+
+merge_directories(core_directory)
 
 if '{{ cookiecutter.has_docker }}' == 'True':
-    merge_directories(docker_directory, target_directory)
+    merge_directories(docker_directory)
