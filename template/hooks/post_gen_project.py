86ffc8890dc2b88836afc5e2cc0b2321fb89d8a0
Matthew Buske <matt@deepworks.net>
2023-11-20 15:38:19 -0500

    Start fixing Readme 'Issue'
    Started to fix the 'readme' issue

---
+++ template/hooks/post_gen_project.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+
+target_directory = '../{{ cookiecutter._template }}{% raw %}{{cookiecutter.project_slug}}/{% endraw %}'
+
+print(f"Starting post-generation script")
+
+
+
--- template/{{cookiecutter.project_slug}}/README.md
+++
@@ -1,2 +0,0 @@
-# Readme
-This file is intentionally blank and will be overridden when generating a project. Committed changes to this file will not really do much.
